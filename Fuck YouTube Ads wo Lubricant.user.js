8e891f870d03430f219339fabda9d290cd681dd0
vippium <meetmevipinprajapat2001@gmail.com>
2025-12-30 17:23:09 +0530

    another direct url update testing

--- Fuck YouTube Ads wo Lubricant.user.js
+++ Fuck YouTube Ads wo Lubricant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fuck YouTube Ads w/o Lubricant
 // @namespace    http://tampermonkey.net/
-// @version      1.5.4
+// @version      1.5.4 - dev2
 // @description  Very Useful for Ad-free experience (M*therF@ckers are not allowed to use this)
 // @author       vippium
 // @match        https://www.youtube.com/*
